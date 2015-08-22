60236a897b0c4ffd75c05c89b11cabaacf152c0a
Matt Sanders <matt@modal.org>
2015-08-22 13:24:01 -0700

    Add support for snapshot #size; start work on percentiles.

--- lib/hetchy/snapshot.rb
+++ lib/hetchy/snapshot.rb
@@ -1,14 +1,37 @@
 class Hetchy
 
   # Contains reservoir data frozen in time, created by Hetchy#snapshot.
+  # This class does only read-only work and should be treated as immutable.
+  #
   class Snapshot
 
-    attr_reader :data
+    attr_reader :data,
+                :size
 
     def initialize(data)
       @data = data.sort
+      @size = @data.length
+    end
+
+    # @return [Float/Integer] median of the data set.
+    def median
+      # TODO
+    end
+
+    # Generate a percentile for the data set.
+    # @example
+    #  snapshot.percentile(95)
+    #  snapshot.percentile(99.9)
+    #
+    def percentile(perc)
+      if perc > 100.0 || perc < 0.0
+        raise InvalidPercentile, "percentile must be between 0.0 and 100.0"
+      end
+
+
     end
 
   end
 
+  class InvalidPercentile < StandardError; end
 end
--- test/unit/snapshot_test.rb
+++ test/unit/snapshot_test.rb
@@ -7,4 +7,19 @@ class SnapshotTest < Minitest::Test
     assert_equal [1,2,3,5], snapshot.data, 'data is sorted'
   end
 
+  def test_percentile
+
+  end
+
+  def test_percentile_limits
+    snapshot = Hetchy::Snapshot.new([1,2,3])
+    assert_raises(Hetchy::InvalidPercentile) { snapshot.percentile(-0.2) }
+    assert_raises(Hetchy::InvalidPercentile) { snapshot.percentile(1000) }
+  end
+
+  def test_size
+    snapshot = Hetchy::Snapshot.new([1,2,3])
+    assert_equal 3, snapshot.size
+  end
+
 end
