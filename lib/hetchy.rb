c68146c4c5e520a8dbc0333ce66f68782b73fa91
Matt Sanders <matt@modal.org>
2015-08-22 12:22:54 -0700

    Get core sampling process working

--- lib/hetchy.rb
+++ lib/hetchy.rb
@@ -8,14 +8,30 @@ class Hetchy
 
   # Available opts:
   # @option opts [Integer] :size Size of reservoir
+  #
   def initialize(opts={})
     @count = 0
     @size = opts.fetch(:size, 1024)
     @pool = Array.new(@size, 0)
   end
 
-  def <<(values)
-    @count += 1
+  # Add one or more values to the reservoir
+  # @example
+  #   reservoir << 1234
+  #   reservoir << [2345,7891,2131]
+  #
+  def << (values)
+    Array(values).each do |value|
+      if count < size
+        @pool[count] = value
+      else
+        index = rand(count+1)
+        if index < @size
+          @pool[index] = value
+        end
+      end
+      @count += 1
+    end
   end
 
 end
--- test/unit/hetchy_test.rb
+++ test/unit/hetchy_test.rb
@@ -2,6 +2,41 @@ require 'test_helper'
 
 class HetchyTest < Minitest::Test
 
+  def test_add_basics
+    # single add
+    reservoir = Hetchy.new(size: 10)
+    reservoir << 1
+    assert_equal 1, reservoir.pool[0]
+
+    # multi add
+    reservoir << [2,3,4]
+    assert_equal 2, reservoir.pool[1]
+    assert_equal 3, reservoir.pool[2]
+    assert_equal 4, reservoir.pool[3]
+  end
+
+  def test_add_saturation_starts_sampling
+    series = Array(1..15)
+    overflow = series[10..-1]
+
+    reservoir = Hetchy.new(size: 10)
+    reservoir << series
+
+    assert_equal 15, reservoir.count, 'all samples recorded'
+    assert_equal 10, reservoir.pool.length, 'pool is stable size'
+    refute_equal (series - reservoir.pool), overflow,
+      'overflow measures are sampled into pool'
+  end
+
+  def test_add_all_pool_accessible
+    series = Array(1..1000)
+    reservoir = Hetchy.new(size: 10)
+    reservoir << series
+
+    refute_equal 1, reservoir.pool[0], 'first entry in pool can be changed'
+    refute_equal 10, reservoir.pool[-1], 'last entry in pool can be changed'
+  end
+
   def test_count
     reservoir = Hetchy.new(size: 10)
     assert_equal 0, reservoir.count
